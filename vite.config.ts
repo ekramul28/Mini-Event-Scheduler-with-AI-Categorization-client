31707ca988c1b6ec0e0499b0e8f1236be465983c
Ekramul <mdekramulhassan168@gmail.com>
2025-07-21 07:45:21 -0700

    added proxy

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,14 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  // vite.config.ts
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://mini-event-with-ai.vercel.app",
+        changeOrigin: true,
+        rewrite: (path: string) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
